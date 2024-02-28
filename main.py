70be5278397322a1774ca8919ebffb4ee9efb8aa
Sunshine0112-Dev <mastersurprise2108@gmail.com>
2024-02-28 10:03:49 -0500

    finish trial

--- main.py
+++ main.py
@@ -8,15 +8,23 @@ from login import *
 # Create a new instance of the Chrome driver
 driver = webdriver.Chrome()
 
+# driver.maximize_window()
+
 # Open the freelancer.com website
 driver.get("https://www.freelancer.com/")
 
-# desplayLogin(driver)
+desplayLogin(driver)
 
 # Hover find job using ActionChains
-hoverable = driver.find_element(By.LINK_TEXT, " Find Jobs ")
-ActionChains(driver).move_to_element(hoverable).perform()
-time.sleep(3)
+hoverable1 = driver.find_element(By.PARTIAL_LINK_TEXT, "Find Jobs")
+# hoverable.click()
+
+ActionChains(driver).move_to_element(hoverable1).perform()
+time.sleep(2)
+
+hoverable2 = driver.find_element(By.PARTIAL_LINK_TEXT, "By Skill")
+ActionChains(driver).move_to_element(hoverable2).perform()
+time.sleep(5)
 
 # Close the browser
 driver.quit()
